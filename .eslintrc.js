d0be37a1b958cced0d6b8c75a58bf007f46ab057
Husain Khambaty <husainkhambaty@users.noreply.github.com>
2024-06-06 18:51:56 +1000

    lint changes

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
     "extends": "standard",
     "rules": {
         "semi": ["error", "always"],
-        "indent": ["error", 2],
+        "indent": ["error", 4],
         "space-before-function-paren": ["error", "never"],
         "no-trailing-spaces": ["error", { "skipBlankLines": true }],
         "no-multiple-empty-lines": ["error", { "max": 4, "maxEOF": 2 }],
--- src/blockchainserver.js
+++ src/blockchainserver.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { Blockchain, Block, Transaction } = require('./blockchain');
+const { Blockchain, Transaction } = require('./blockchain');
 const app = express();
 const PORT = 3000;
 const EC = require('elliptic').ec;
@@ -7,7 +7,7 @@ const ec = new EC('secp256k1');
 
 // Start to listen at the app port
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
 
 app.use(express.json());
@@ -17,64 +17,64 @@ let tbbChain = new Blockchain();
 let walletLoadStatus = {};
 
 app.post('/loadWallet', (req, res) => {
-    let myWalletAddress = req.body.myWalletAddress;
+  let myWalletAddress = req.body.myWalletAddress;
     
-    // Load it once
-    if (!walletLoadStatus[myWalletAddress]) {
-        tbbChain.minePendingTransactions(myWalletAddress); walletLoadStatus[myWalletAddress] = true;
-    }
+  // Load it once
+  if (!walletLoadStatus[myWalletAddress]) {
+    tbbChain.minePendingTransactions(myWalletAddress); walletLoadStatus[myWalletAddress] = true;
+  }
 });
 
 // API to create a new transaction
 app.post('/createTransaction', (req, res) => {
-    let data = req.body;
-    let myEncryptedKey = ec.keyFromPrivate(data.myKey);
+  let data = req.body;
+  let myEncryptedKey = ec.keyFromPrivate(data.myKey);
 
-    // TODO: Validate all data
+  // TODO: Validate all data
 
-    // Load it once
-    if (!walletLoadStatus[data.myWalletAddress]) {
-        tbbChain.minePendingTransactions(data.myWalletAddress); walletLoadStatus[data.myWalletAddress] = true;
-    }
-    
-    console.log(`Creating a txn from ${data.myWalletAddress} to ${data.receiverWalletAddress} for ${data.amount}`);
+  // Load it once
+  if (!walletLoadStatus[data.myWalletAddress]) {
+    tbbChain.minePendingTransactions(data.myWalletAddress); walletLoadStatus[data.myWalletAddress] = true;
+  }
+  
+  console.log(`Creating a txn from ${data.myWalletAddress} to ${data.receiverWalletAddress} for ${data.amount}`);
 
-    // create a txn and sign it
-    let txn = new Transaction(data.myWalletAddress, data.receiverWalletAddress, data.amount)
-    txn.sign(myEncryptedKey);
+  // create a txn and sign it
+  let txn = new Transaction(data.myWalletAddress, data.receiverWalletAddress, data.amount)
+  txn.sign(myEncryptedKey);
 
-    // catch errors whie adding the txn to the blockchain
-    try {
-       tbbChain.addTransaction(txn);
-    } catch(err) {
-        console.log("Failed to add transaction");
-        res.status(500).send({"message" : "Not enough balance"});
-    } 
-    console.log("Transaction created and added to blockchain");
+  // catch errors whie adding the txn to the blockchain
+  try {
+    tbbChain.addTransaction(txn);
+  } catch(err) {
+    console.log("Failed to add transaction");
+    res.status(500).send({"message" : "Not enough balance"});
+  } 
+  console.log("Transaction created and added to blockchain");
 
-    // mine the block
-    console.log("Mine the block");
-    tbbChain.minePendingTransactions(data.myWalletAddress);
-    
-    res.status(200).send({"message" : "Transaction created successfully"});
+  // mine the block
+  console.log("Mine the block");
+  tbbChain.minePendingTransactions(data.myWalletAddress);
+  
+  res.status(200).send({"message" : "Transaction created successfully"});
 
 });
 
 app.get('/getChain', (req, res) => {
-    res.status(200).send(tbbChain.chain);
+  res.status(200).send(tbbChain.chain);
 });
 
 app.get('/getPublicKey', (req, res)=> {
-    let pvtKey = req.body.pvtKey;
-    res.status(200).send({'publicKey': ec.keyFromPrivate(pvtKey).getPublic('hex')});
+  let pvtKey = req.body.pvtKey;
+  res.status(200).send({'publicKey': ec.keyFromPrivate(pvtKey).getPublic('hex')});
 });
 
 app.get('/getBalance', (req, res) => {
-    let myWalletAddress = req.body.myWalletAddress;
-    res.status(200).send({"balance" : tbbChain.getBalanceOfAddress(myWalletAddress)});
+  let myWalletAddress = req.body.myWalletAddress;
+  res.status(200).send({"balance" : tbbChain.getBalanceOfAddress(myWalletAddress)});
 });
 
 app.get('/viewWalletTransactions', (req, res) => {
-    let myWalletAddress = req.body.myWalletAddress;
-    res.status(200).send({"transactions" : tbbChain.getAllTransactionsForWallet(myWalletAddress)});
+  let myWalletAddress = req.body.myWalletAddress;
+  res.status(200).send({"transactions" : tbbChain.getAllTransactionsForWallet(myWalletAddress)});
 });
